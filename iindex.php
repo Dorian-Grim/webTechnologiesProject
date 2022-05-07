49258bcba4ab4f51361682d1daec2ce5b9095360
Dorian <dorian.buraga@outlook.com>
2022-05-07 12:16:02 +0300

    html skeleton

--- iindex.php
+++ iindex.php
@@ -1,3 +1,24 @@
 <?php
-echo "test";
+echo <<<HTML
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="index.css">
+        <title>Index</title>
+    </head>
+    <body>
+        <h1>Index</h1>
+        <p>
+            <a href="create.php">Create</a>
+        </p>
+        <p>
+            <a href="read.php">Read</a>
+        </p>
+        <p>
+
+        </p>
+    </body>
+    </html>
+HTML;
 ?>
